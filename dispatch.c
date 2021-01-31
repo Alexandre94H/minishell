09e6e15c73ff23af4075082df9c8188be3e4766c
Alexandre Hallaine <ahallain@student.42.fr>
2021-01-31 15:14:13 +0100

    add: command separator ';'

--- dispatch.c
+++ dispatch.c
@@ -6,25 +6,49 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/30 10:18:13 by ahallain          #+#    #+#             */
-/*   Updated: 2021/01/30 22:56:30 by ahallain         ###   ########.fr       */
+/*   Updated: 2021/01/31 15:11:08 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 #include <stdlib.h>
 #include "default.h"
 #include "utils/lib.h"
 
-int	dispatch(char *content, char **env)
+int	run(char *content, char **env, int last_output)
 {
-	int			ret;
-	char		**args;
-	static int	last_output;
+	char	**args;
+	size_t	index;
+	int		ret;
 
 	args = ft_split(content, ' ');
 	ret = call_function(args, env, last_output);
 	if (ret > 255)
 		ret = execute(args, env);
+	index = 0;
+	while (args[index])
+		free(args[index++]);
 	free(args);
+	return (ret);
+}
+
+int	dispatch(char *content, char **env)
+{
+	char		**contents;
+	size_t		index;
+	int			ret;
+	static int	last_output;
+
+	contents = ft_split(content, ';');
+	index = 0;
+	while (contents[index])
+	{
+		ret = run(contents[index], env, last_output);
+		free(contents[index++]);
+		if (ret < 0)
+			break ;
+	}
+	free(contents);
 	if (ret > 255)
 		ft_putstr("command not found\n");
 	else if (ret < 0)
