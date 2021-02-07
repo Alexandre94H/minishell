339c52dccdd35e263ffa38a85c489d61a421c391
Alexandre Hallaine <ahallain@student.42.fr>
2021-02-07 12:24:43 +0100

    update: simplify cd

--- functions/f_cd.c
+++ functions/f_cd.c
@@ -6,113 +6,29 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 10:23:35 by ahallain          #+#    #+#             */
-/*   Updated: 2021/02/06 12:47:34 by ahallain         ###   ########.fr       */
+/*   Updated: 2021/02/07 12:24:03 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <unistd.h>
 #include "../utils/env.h"
 #include "../utils/lib.h"
 
-char	*get_parent(char *path)
-{
-	size_t	index;
-	size_t	length;
-	char	*parent;
-	char	**split;
-
-	split = ft_split(path, '/');
-	length = 0;
-	while (split[length])
-		length++;
-	if (!(parent = malloc(sizeof(char *))))
-		return (0);
-	*parent = 0;
-	index = 0;
-	while (index < length)
-	{
-		ft_stradd(&parent, "/");
-		if (index != length - 1)
-			ft_stradd(&parent, split[index]);
-		free(split[index]);
-		index++;
-	}
-	if (!length)
-		ft_stradd(&parent, "/");
-	free(split);
-	return (parent);
-}
-
-char	init_path(char *arg, char *current, char **new)
-{
-	char	*temp;
-	char	ret;
-
-	if (*arg == '/')
-		return (0);
-	if (ft_equals(arg, "..") || !ft_stristr(arg, "../"))
-	{
-		temp = get_parent(current);
-		ft_stradd(new, temp);
-		free(temp);
-		return (2);
-	}
-	if (ft_equals(arg, ".") || !ft_stristr(arg, "./"))
-		ret = 1;
-	else
-		ret = 0;
-	ft_stradd(new, current);
-	return (ret);
-}
-
-void	change_path(char *arg, char *new, char **env)
-{
-	if (ft_equals(arg, "/"))
-		ft_stradd(&new, arg);
-	else
-	{
-		if (*arg == '/')
-			arg++;
-		if (*arg)
-		{
-			if (new[ft_strlen(new, 0) - 1] != '/')
-				ft_stradd(&new, "/");
-			ft_stradd(&new, arg);
-		}
-	}
-	if (chdir(new) == -1)
-		ft_putstr_fd("invalid path\n", 2);
-	else
-	{
-		env_set(env, "OLDPWD", env_get(env, "PWD"));
-		env_set(env, "PWD", new);
-	}
-	free(new);
-}
-
 char	f_cd(char **args, char **env)
 {
-	char	*new;
-	char	*arg;
+	char	*home;
 
-	if (!args[1])
-	{
-		ft_putstr_fd("need a path\n", 2);
+	if (!args[1] || ft_equals(args[1], "--"))
+		home = env_get(env, "HOME");
+	else if (ft_equals(args[1], "-"))
+		home = env_get(env, "OLDPWD");
+	else
+		home = args[1];
+	if (chdir(home) == -1)
 		return (1);
-	}
-	if (args[2])
-	{
-		ft_putstr_fd("too many arguments\n", 2);
+	if (!(home = getcwd(NULL, 0)))
 		return (1);
-	}
-	arg = args[1];
-	if (ft_equals(arg, "-"))
-		arg = env_get(env, "OLDPWD");
-	if (!(new = malloc(sizeof(char *))))
-		return (1);
-	*new = 0;
-	arg += init_path(arg, env_get(env, "PWD"), &new);
-	change_path(arg, new, env);
+	env_set(env, "OLDPWD", env_get(env, "PWD"));
+	env_set(env, "PWD", home);
 	return (0);
 }
