925ba8128e878bf130f5d4dc0a8df374ddd78e77
Alexandre Hallaine <ahallain@student.42.fr>
2021-02-07 12:29:29 +0100

    add: cd errors

--- functions/f_cd.c
+++ functions/f_cd.c
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 10:23:35 by ahallain          #+#    #+#             */
-/*   Updated: 2021/02/07 12:24:03 by ahallain         ###   ########.fr       */
+/*   Updated: 2021/02/07 12:28:57 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ char	f_cd(char **args, char **env)
 {
 	char	*home;
 
+	if (args[1] && args[2])
+	{
+		ft_putstr_fd("too many arguments\n", 2);
+		return (1);
+	}
 	if (!args[1] || ft_equals(args[1], "--"))
 		home = env_get(env, "HOME");
 	else if (ft_equals(args[1], "-"))
@@ -25,7 +30,10 @@ char	f_cd(char **args, char **env)
 	else
 		home = args[1];
 	if (chdir(home) == -1)
+	{
+		ft_putstr_fd("no such file or directory\n", 2);
 		return (1);
+	}
 	if (!(home = getcwd(NULL, 0)))
 		return (1);
 	env_set(env, "OLDPWD", env_get(env, "PWD"));
