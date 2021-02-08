3a7eb34cd761f9584e0a598e149cc1e6672ed625
Alexandre Hallaine <ahallain@student.42.fr>
2021-02-08 16:34:27 +0100

    fix: arrow compilation fail

--- srcs/arrow.c
+++ srcs/arrow.c
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 11:50:30 by ahallain          #+#    #+#             */
-/*   Updated: 2021/02/07 17:09:36 by ahallain         ###   ########.fr       */
+/*   Updated: 2021/02/08 16:33:38 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int	arrow(char *prefix, char *file)
 {
 	int	fd;
 
+	fd = -2;
 	if (*prefix == '<' && prefix[1] != '<')
 		fd = open(file, O_RDONLY);
 	else if (*prefix == '>')
@@ -41,8 +42,6 @@ int	arrow(char *prefix, char *file)
 			fd = open(file, O_WRONLY | O_CREAT | O_TRUNC,
 				S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	}
-	else
-		fd = -2;
 	if (fd < 0)
 		return (fd);
 	if (init_std(fd, *prefix == '<') == -1)
